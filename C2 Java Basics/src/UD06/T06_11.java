244043f8b99b27e225b696dc25374ae38604f77b
thefishgluglu <danielfco.garciacuesta@gmail.com>
2025-03-07 12:44:03 +0100

    tareas grupales

--- C2 Java Basics/src/UD06/T06_11.java
+++ C2 Java Basics/src/UD06/T06_11.java
@@ -1,62 +1,75 @@
+
 package UD06;
 
 import javax.swing.JOptionPane;
 
 public class T06_11 {
 
-	public static void main(String[] args) {
-		String Filas = JOptionPane.showInputDialog("Inserte el numero de filas de la matriz: ");
-		String Columnas = JOptionPane.showInputDialog("Inserte el numero de columnas de la matriz: ");
+  public static void main(String[] args) {
+    // Solicita al usuario el número de filas y columnas para la matriz
+    String Filas = JOptionPane.showInputDialog("Inserte el numero de filas de la matriz: ");
+    String Columnas = JOptionPane.showInputDialog("Inserte el numero de columnas de la matriz: ");
 
-		int array[][] = new int[Integer.parseInt(Filas)][Integer.parseInt(Columnas)];
+    // Inicializa dos matrices con las dimensiones especificadas por el usuario
+    int array[][] = new int[Integer.parseInt(Filas)][Integer.parseInt(Columnas)];
+    int array2[][] = new int[Integer.parseInt(Filas)][Integer.parseInt(Columnas)];
 
-		int array2[][] = new int[Integer.parseInt(Filas)][Integer.parseInt(Columnas)];
-		int i = 0;
-		while (i < Integer.parseInt(Filas)) {
-			int j = 0;
-			while (j < Integer.parseInt(Columnas)) {
-				array[i][j] = ValorRand();
-				j++;
-			}
-			i++;
-		}
-		array2 = array;
-		System.out.println("Array 1");
-		Mostrar(array, array.length, array[0].length);
-		System.out.println("Array 2");
-		Mostrar(array2, array2.length, array2[0].length);
-		int[][] array3 = new int[Integer.parseInt(Filas)][Integer.parseInt(Columnas)];
-		System.out.println("Array 3");
-		array3 = multipliArrays(array, array2, array3, array.length, array[0].length);
-
-	}
-
-	public static int ValorRand() {
-		int RandomNum = (int) (Math.random() * 10);
-		return RandomNum;
-	}
-
-	public static int Mostrar(int[][] num, int Longitud, int Longitud2) {
-		for (int i = 0; i < Longitud; i++) {
-			for (int j = 0; j < Longitud2; j++) {
-				System.out.println("El valor de num[" + i + "][" + j + "] es " + num[i][j]);
-//			System.out.println("El valor de num[" + i + "] es " + num[i][j]);
-			}
-		}
-		return 0;
-	}
-
-	public static int[][] multipliArrays(int Array1[][], int Array2[][],int Array3[][], int Filas, int Columnas) {
-		   
-        for (int i = 0; i < Filas; i++) {
-            for (int j = 0; j < Columnas; j++) {
-                Array3[i][j] = Array1[i][j] * Array2[i][j];
-				System.out.println("El valor de num[" + i + "][" + j + "] es " + Array3[i][j]);
-            }
-        }
-        
-        
-		return Array3;
+    // Llena la primera matriz con valores aleatorios
+    int i = 0;
+    while (i < Integer.parseInt(Filas)) {
+      int j = 0;
+      while (j < Integer.parseInt(Columnas)) {
+        array[i][j] = ValorRand();
+        j++;
+      }
+      i++;
     }
+
+    // Copia los valores de la primera matriz a la segunda
+    array2 = array;
+
+    // Muestra los valores de la primera matriz
+    System.out.println("Array 1");
+    Mostrar(array, array.length, array[0].length);
+
+    // Muestra los valores de la segunda matriz
+    System.out.println("Array 2");
+    Mostrar(array2, array2.length, array2[0].length);
+
+    // Inicializa una tercera matriz para almacenar el resultado de la multiplicación
+    int[][] array3 = new int[Integer.parseInt(Filas)][Integer.parseInt(Columnas)];
+    System.out.println("Array 3");
+
+    // Multiplica los valores de las dos primeras matrices y almacena el resultado en la tercera
+    array3 = multipliArrays(array, array2, array3, array.length, array[0].length);
+  }
+
+  // Genera un número aleatorio entre 0 y 9
+  public static int ValorRand() {
+    int RandomNum = (int) (Math.random() * 10);
+    return RandomNum;
+  }
+
+  // Muestra los valores de una matriz
+  public static int Mostrar(int[][] num, int Longitud, int Longitud2) {
+    for (int i = 0; i < Longitud; i++) {
+      for (int j = 0; j < Longitud2; j++) {
+        System.out.println("El valor de num[" + i + "][" + j + "] es " + num[i][j]);
+      }
+    }
+    return 0;
+  }
+
+  // Multiplica los valores de dos matrices y almacena el resultado en una tercera matriz
+  public static int[][] multipliArrays(int Array1[][], int Array2[][], int Array3[][], int Filas, int Columnas) {
+    for (int i = 0; i < Filas; i++) {
+      for (int j = 0; j < Columnas; j++) {
+        Array3[i][j] = Array1[i][j] * Array2[i][j];
+        System.out.println("El valor de num[" + i + "][" + j + "] es " + Array3[i][j]);
+      }
+    }
+    return Array3;
+  }
 }
+
 		
--- C2 Java Basics/src/UD06/T06_12.java
+++ C2 Java Basics/src/UD06/T06_12.java
@@ -1,47 +1,81 @@
+
 package UD06;
+
 import javax.swing.JOptionPane;
-public class T06_12 {	
 
+public class T06_12 {
 
-		public static void main(String[] args) {
-			String Filas = JOptionPane.showInputDialog("Inserte el numero de filas de la matriz: ");
-			String Columnas = JOptionPane.showInputDialog("Inserte el numero de columnas de la matriz: ");
-			String cifra = JOptionPane.showInputDialog("Inserte la cifra final deseada: ");
+  public static void main(String[] args) {
+    // Solicita al usuario el número de filas y columnas para la matriz
+    String Filas = JOptionPane.showInputDialog("Inserte el numero de filas de la matriz: ");
+    String Columnas = JOptionPane.showInputDialog("Inserte el numero de columnas de la matriz: ");
+    String cifra = "";
 
-			int array[][] = new int[Integer.parseInt(Filas)][Integer.parseInt(Columnas)];
-			int i = 0;
-			while (i < Integer.parseInt(Filas)) {
-				int j = 0;
-				while (j < Integer.parseInt(Columnas)) {
-					array[i][j] = ValorRand();
-					j++;
-				}
-				i++;
-			}
-			System.out.println("Array 1");
-			mostrar(array, array.length, array[0].length, Integer.parseInt(cifra));
-			
-		}public static int ValorRand() {
-			int RandomNum = (int) (Math.random() * 300);
-			return RandomNum;
-		
-	}
-//		public static int Mostrar(int[][] num, int Longitud, int Longitud2) {
-//		    for (int i = 0; i < Longitud; i++) {
-//		        for (int j = 0; j < Longitud2; j++) {
-//		            System.out.println("El valor de num[" + i + "][" + j + "] es " + num[i][j]);
-//		        }
-//		    }
-//		    return 0;
-//		}
-		public static void mostrar(int[][] num, int longitud, int longitud2, int cifra) {
-	        for (int i = 0; i < longitud; i++) {
-	            for (int j = 0; j < longitud2; j++) {
-	                if (num[i][j] % 10 == cifra) {
-	                    System.out.println("El valor de num[" + i + "][" + j + "] es " + num[i][j]);
-	                }
-	            }
-	        }
-	    }
+    // Inicializa la matriz con las dimensiones especificadas por el usuario
+    int array[][] = new int[Integer.parseInt(Filas)][Integer.parseInt(Columnas)];
+    int i = 0;
+
+    // Filtro para que el usuario introduzca un número mayor que 0 y menos de 10, es decir, un número de una cifra
+    do {
+      cifra = JOptionPane.showInputDialog("Inserte la cifra final deseada: ");
+    } while (Integer.parseInt(cifra) < 0 || Integer.parseInt(cifra) > 9);
+
+    // Llena la matriz con valores aleatorios
+    while (i < Integer.parseInt(Filas)) {
+      int j = 0;
+      while (j < Integer.parseInt(Columnas)) {
+        array[i][j] = ValorRand();
+        j++;
+      }
+      i++;
+    }
+
+    // Copia los valores de la primera matriz a una segunda matriz
+    int[][] array2 = new int[array.length][array[0].length];
+    for (i = 0; i < array.length; i++) {
+      for (int j = 0; j < array[i].length; j++) {
+        array2[i][j] = array[i][j];
+      }
+      System.out.println();
+    }
+
+    // Muestra los valores de la primera matriz
+    System.out.println("Array 1");
+    Mostrar1(array, array.length, array[0].length, i);
+
+    // Muestra los valores de la segunda matriz que cumplen con la condición de la cifra final
+    System.out.println("Array 2");
+    mostrar2(array, array.length, array[0].length, Integer.parseInt(cifra));
   }
+
+  // Genera un número aleatorio entre 0 y 299
+  public static int ValorRand() {
+    int RandomNum = (int) (Math.random() * 300);
+    return RandomNum;
+  }
+
+  // Muestra los valores de una matriz
+  public static int Mostrar1(int[][] num, int Longitud, int Longitud2, int i2) {
+    for (int i = 0; i < Longitud; i++) {
+      for (int j = 0; j < Longitud2; j++) {
+        System.out.print(num[i][j] + " ");
+      }
+      System.out.println();
+    }
+    return 0;
+  }
+
+  // Muestra los valores de una matriz que cumplen con la condición de la cifra final
+  public static void mostrar2(int[][] num, int longitud, int longitud2, int cifra) {
+    for (int i = 0; i < longitud; i++) {
+      for (int j = 0; j < longitud2; j++) {
+        if (num[i][j] % 10 == cifra) {
+          System.out.println("El valor de num[" + i + "][" + j + "] es " + num[i][j]);
+        } else {
+          System.out.println("no hay ningun " + cifra);
+        }
+      }
+    }
+  }
+}
 
