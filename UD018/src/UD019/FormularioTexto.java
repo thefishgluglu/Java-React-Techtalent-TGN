697bd451605410e71dd49dd97c0becc7fe823f24
thefishgluglu <danielfco.garciacuesta@gmail.com>
2025-05-29 13:36:06 +0200

    UD019

---
+++ UD018/src/UD019/FormularioTexto.java
@@ -0,0 +1,40 @@
+package UD019;
+
+	import javax.swing.*;
+	import java.awt.*;
+	import java.awt.event.*;
+
+	public class FormularioTexto extends JFrame {
+	    private JTextField campoTexto;
+	    private JButton botonMostrar;
+
+	    public FormularioTexto() {
+	        setTitle("Ejercicio 1 - UD19");
+	        setSize(400, 150);
+	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        setLayout(new FlowLayout());
+
+	        JLabel etiqueta = new JLabel("Introduce un texto:");
+	        campoTexto = new JTextField(20);
+	        botonMostrar = new JButton("Mostrar");
+
+	        // Evento al hacer clic en el botón
+	        botonMostrar.addActionListener(new ActionListener() {
+	            public void actionPerformed(ActionEvent e) {
+	                String texto = campoTexto.getText();
+	                JOptionPane.showMessageDialog(null, "Texto ingresado: " + texto);
+	            }
+	        });
+
+	        add(etiqueta);
+	        add(campoTexto);
+	        add(botonMostrar);
+	    }
+	    public static void main(String[] args) {
+	        FormularioTexto ventana = new FormularioTexto();
+	        ventana.setVisible(true);
+	    }
+
+	    
+	}
+
